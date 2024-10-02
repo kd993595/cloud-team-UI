1dfeb90adffb62f94ebdc1fa9ac8df5c3271906f
gruiiiw <107224535+gruiiiw@users.noreply.github.com>
2024-10-02 17:42:18 -0400

    update signup api

--- src/pages/Signup.tsx
+++ src/pages/Signup.tsx
@@ -1,25 +1,18 @@
 import React, { useState } from 'react';
 import StickyBar from "../components/StickyBar.tsx";
+import { userSignup } from '../utils/api.tsx';
 
 function Signup() {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    const [username, setUsername] = useState('');
 
     const handleSignup = async (event: React.FormEvent) => {
         event.preventDefault();
         
         try {
             console.log("submitting");
-            const response = await fetch('http://127.0.0.1:5000/signup', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify({
-                    user_email: email,
-                    password: password,
-                }),
-            });
+            const response = await userSignup(username, email, password); 
             console.log(response);
 
             if (response.ok) {
@@ -37,7 +30,21 @@ function Signup() {
         <>
             <StickyBar />
             <h2>Signup</h2>
-            <form onSubmit={handleSignup} id = "login-signup-form">
+            <form onSubmit={handleSignup} id="login-signup-form">
+                <div>
+                    <div>
+                        <label htmlFor="username"><b>Username: </b></label>
+                    </div>
+                    <div>
+                        <input
+                            type="text"
+                            id="username"
+                            value={username}
+                            onChange={(e) => setUsername(e.target.value)}
+                            required
+                        />
+                    </div>
+                </div>
                 <div>
                     <div>
                         <label htmlFor="email"><b>Email: </b></label>
