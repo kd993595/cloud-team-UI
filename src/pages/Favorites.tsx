ec2f8bc698b245983da60ff5ef5f687b1824e346
gruiiiw <107224535+gruiiiw@users.noreply.github.com>
2024-10-11 19:06:09 -0400

    Favorites updates

--- src/pages/Favorites.tsx
+++ src/pages/Favorites.tsx
@@ -1,12 +1,38 @@
 import StickyBar from "../components/StickyBar.tsx";
+import { getPreferences } from '../utils/api.tsx';
+import { useState, useEffect } from 'react';
 
 function Favorites() {
+    const [preferences, setPreferences] = useState([]);
+
+    // currently preferences is requiring the user ID to be passed in
+
+    useEffect(() => {
+        const getPreferences = async () => {
+            try {
+                const data = await getPreferences();
+                setPreferences(data ?? []);
+            } catch (error) {
+                console.error(error);
+            }
+        };
+
+        getPreferences();
+    }, []);
 
     return (
         <>
             <StickyBar/>
-            <p>Favorites is under construction!</p>
-        </>
+            <h2>Favorites</h2>
+            {preferences.length > 0 ? (
+                <ul>
+                    {preferences.map((pref, index) => (
+                        <li key={index}>{pref}</li>
+                    ))}
+                </ul>
+            ) : (
+                <p>No favorites found.</p>
+            )}        </>
     )
 }
 
