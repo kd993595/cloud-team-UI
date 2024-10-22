517ee7dfd96c8993c6978054d24c46e4cd815f5e
Grace Dong <grace.r.dong@gmail.com>
2024-10-22 16:27:45 -0400

    Add way to favorite items-> changed CSS
    TBA: connection with the Preference API

--- src/components/DiningHall.tsx
+++ src/components/DiningHall.tsx
@@ -1,8 +1,12 @@
 import {Food} from "../types/types.ts";
 import FoodCard from "./FoodCard.tsx";
-import {ReactElement} from "react";
+import {ReactElement, useState} from "react";
 
 function DiningHall({name, allFood}: {name: string; allFood: Food[]}) {
+    // Favorites is a list of all the food items that the user has favorited
+    const [favorites, setFavorites] = useState<Food[]>([]);
+    
+
     const menus = new Map<string, Food[]>;
     // Sort all food into its menu bucket
     allFood.forEach((food) => {
@@ -11,9 +15,33 @@ function DiningHall({name, allFood}: {name: string; allFood: Food[]}) {
         menus.get(food.meal)!.push(food);
     })
 
+    const updateFavoriteStatus = async (food: Food, isFavorite: boolean) => {
+        // This function should send a request to the API to update the favorite status of the food
+    };
+
+    const toggleFavorite = (food: Food) => {
+        const isFavorite = favorites.includes(food);
+        setFavorites((prevFavorites) => {
+            if (isFavorite) {
+                return prevFavorites.filter(fav => fav !== food);
+            } else {
+                return [...prevFavorites, food];
+            }
+        });
+        updateFavoriteStatus(food, isFavorite);
+    };
+
     const menuComponents: ReactElement[] = [];
     menus.forEach((food, name) => {
-        menuComponents.push(<Menu name={name} foodArray={food} key={name}/>);
+        menuComponents.push(
+            <Menu 
+                name={name} 
+                foodArray={food} 
+                key={name}
+                favorites = {favorites}
+                toggleFavorite={toggleFavorite}
+                />
+        );
     })
 
     return (
@@ -24,12 +52,27 @@ function DiningHall({name, allFood}: {name: string; allFood: Food[]}) {
     )
 }
 
-function Menu({name, foodArray}: {name: string; foodArray: Food[]}) {
+function Menu({
+        name, 
+        foodArray,
+        favorites,
+        toggleFavorite,
+    }: {
+        name: string; 
+        foodArray: Food[];
+        favorites: Food[];
+        toggleFavorite: (food: Food) => void;}) {
     return (
         <div>
             <h3>{name}</h3>
             {foodArray.length > 0 ?
-                foodArray.map((food) => (<FoodCard food={food} key={food.food_name}/>))
+                foodArray.map((food) => (
+                    <FoodCard 
+                        food={food} 
+                        key={food.food_name}
+                        isFavorite={favorites.includes(food)}
+                        toggleFavorite={toggleFavorite}
+                    />))
                 : "Closed"
             }
         </div>
